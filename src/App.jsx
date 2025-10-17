80d504e964c9cb221037daf2cfe8ec2e63328756
Arvin Jafary <arwinux@gmail.com>
2025-10-17 23:05:49 +0330

    start project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <p className="text-blue-600 font-bold" bg-slate-800>
+        این شروع یک حرفه است
+      </p>
+    </div>
+  );
+}
+
+export default App;
