f57a8551b643ca1746aa2d7f9d13b60a0f75efcf
Arvin Jafary <arwinux@gmail.com>
2025-10-25 17:16:13 +0330

    feat(otp-auth): add Auth page & react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="">
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
       <Routes>
         <Route path="/auth" element={<Auth />} />
       </Routes>
-    </div>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,9 @@
+function Auth() {
+  return <div className="bg-gray-200"></div>;
+}
+
+export default Auth;
+
+//? step => 1 : ?
+//* send OTP
+//^ check OTP
